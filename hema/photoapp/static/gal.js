8c546351f831672d869af621c7ea20181b8cc655
HemalishaTagore <hemalishatagore07@gmail.com>
2025-10-08 00:35:16 +0530

    Success

---
+++ hema/photoapp/static/gal.js
@@ -0,0 +1,9 @@
+function mousein(img) {
+    img.style.width = "230px";
+    img.style.height = "320px";
+}
+
+function mouseout(img) {
+    img.style.width = "210px";
+    img.style.height = "280px";
+}
